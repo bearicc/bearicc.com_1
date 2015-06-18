62f82e1063e191c2f98ed76f0145343359d0cae1
bearicc <hxiongustc@gmail.com>
2015-06-17 22:26:11 -0500

    add footer art

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,18 @@
 $(document).ready(function () {
+  // dynamic settings
   if($("body").height() < $("html").height()) {
     $("html, body").css({"height": "100%"});
   }
+  set_footer_art();
+
+  // event
+  $(window).resize(function () {
+    set_footer_art();
+  });
 });
+
+function set_footer_art() {
+    var footer_height = $("footer").height();
+    console.log(footer_height);
+    $(".main-container").css({"padding-bottom": footer_height+20});
+}
